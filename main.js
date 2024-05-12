38ea81f5f66f351ffb536fc6e9d4981a5f86a2cc
cole92 <aleksandar9212@gmail.com>
2024-05-12 09:38:56 +0200

    Basic functionality of the application is completed. Now moving on to some optimization and maybe adding new features.

---
+++ main.js
@@ -0,0 +1,29 @@
+// Event listener na dugme 
+document.getElementById('getQuoteBtn').addEventListener('click', () => {
+
+    const apiUrl = 'https://api.quotable.io/random';
+
+    fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error (`Error: Api call failed with status ${response.status} and status text ${response.statusText}`)
+        }
+        return response.json()
+    })
+    .then(data => {
+        const dataContent = data.content;
+        const dataAuthor = data.author;
+
+        const quoteElement = document.getElementById('quote');
+        const authorElement = document.getElementById('author');
+
+        quoteElement.textContent = `${dataContent}`;
+        authorElement.textContent = `${dataAuthor}`;
+    })
+    .catch(error => {
+        console.log('Error while getting data', error);
+        quoteElement.textContent = `Failed to fetch quote, please try again later.`
+        authorElement.textContent = ''
+    })
+
+})
