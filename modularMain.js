945a42563372b176a117551249664e96544aa42c
cole92 <aleksandar9212@gmail.com>
2024-05-15 12:35:00 +0200

    Finished modular and done some local storage

--- modularMain.js
+++ modularMain.js
@@ -2,14 +2,16 @@
 const setEventListeners = () => {
     document.getElementById('getQuoteBtn').addEventListener('click', fetchAndDisplayQuote);
 };
-// Funkcija za dovavljanje i prikaz podataka
+// Funkcija za dobavljanje i prikaz podataka
 const fetchAndDisplayQuote = () => {
     const quoteUrl = 'https://api.quotable.io/random';
 
     fetch(quoteUrl)
     .then(handleResponse)
-    .then(updateUI)
-    .catch(handleError);
+    .then(data => {
+        updateUI(data);
+        saveQuoteToLocal(data); // Čuvanje novog citata u lokalno skladište
+    })
 };
 // Funkcija za obradom odgovora
 const handleResponse = response => {
@@ -29,6 +31,14 @@ const handleError = error => {
     document.getElementById('quote').textContent = 'Failed to fetch quote, please try again later.';
     document.getElementById('author').textContent = '';
 };
+// Funkcija za cuvanje citata u lokalnom skladistu
+const saveQuoteToLocal = quote => {
+    let quotes = JSON.parse(localStorage.getItem('quotes')) || [];
+    quotes.push(quote);
+    localStorage.setItem('quotes', JSON.stringify(quotes));
+    console.log('Quotes after saving:', JSON.parse(localStorage.getItem('quotes')));
+};
+
 // Funkcija za inicijalizovanje - pokretanje aplikacije
 const init = () => {
     setEventListeners();
